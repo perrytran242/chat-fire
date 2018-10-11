27a59be806299a9874b93801c3e9e9a760731108
Perry Tran <ptran209@gmail.com>
2018-10-11 16:11:47 -0700

    Project setup complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import { BrowserRouter as Router} from 'react-router-dom';
+import rootReducer from './reducers'
+import think from './middleware/think';
 import App from './components/app';
 
+const store = createStore(rootReducer, {}, applyMiddleware(think))
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
