fd707b15155b14a1c8e116ec3e36263937a08602
Perry Tran <ptran209@gmail.com>
2018-10-11 18:06:32 -0700

    Connected Firebase chat in progress

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import chatReducer from './chat_reducer';
 
 const rootReducer = combineReducers({
-    chatReducer: chatReducer,
+    chat: chatReducer,
 })
 
 export default rootReducer;
